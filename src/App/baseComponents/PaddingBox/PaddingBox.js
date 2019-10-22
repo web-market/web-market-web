7e5990f9614c071d64c6ee39dccc1794c2029304
dkostreba <kostrebadd@gmail.com>
2019-10-22 18:24:23 +0300

    Paddingbox

--- src/App/baseComponents/PaddingBox/PaddingBox.js
+++ src/App/baseComponents/PaddingBox/PaddingBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import classNames from 'classnames';
-import styles from './styles';
+import styles from './styles/index.scss';
 
 const PaddingBox = ({ children,
                         tiny,
