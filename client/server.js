bcc3379c2bed90782f7d8f9a7fbdb74acd8399d8
dkostreba <kostrebadd@gmail.com>
2019-10-29 23:13:18 +0200

    app

--- client/server.js
+++ client/server.js
@@ -3,17 +3,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname + '/src')));
-app.use('/styles', express.static(__dirname));
-app.use('/images', express.static(__dirname + '/images'));
-app.use('/scripts', express.static(__dirname + '/scripts'));
+app.use(express.static('dist'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + 'src/index.html'));
-});
-
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname + 'src/about.html'));
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
