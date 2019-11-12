b86b1fbaab53cc554c2bd4306b8f3c53e8e6efc4
dkostreba <kostrebadd@gmail.com>
2019-11-12 22:08:09 +0200

    make it better

--- client/server.js
+++ client/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client/dist', 'index.html'));
+	res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
