dfe66301dabbf970b9f4fab10e92a59294780af5
dkostreba <kostrebadd@gmail.com>
2019-12-03 22:35:34 +0200

    Add category.

--- client/src/components/Admin/Category/component/AddCategoryForm.js
+++ client/src/components/Admin/Category/component/AddCategoryForm.js
@@ -2,30 +2,50 @@ import React from 'react';
 import Form, { Field } from '../../../../baseComponents/Form';
 import Textbox from '../../../../baseComponents/Form/Adapters/Textbox';
 import Dropdown from '../../../../baseComponents/Form/Adapters/Dropdown';
-import FormLayout, { FormLayoutItem } from '../../../../baseComponents/FormLayout';
+import FormLayout, { FormLayoutHeader, FormLayoutFooter, FormLayoutItem, FormLayoutItemGroup } from '../../../../baseComponents/FormLayout';
+import ButtonGroup from '../../../../baseComponents/ButtonGroup';
+import Button from '../../../../baseComponents/Button';
 
 // import PropTypes from 'prop-types';
 
 import { ADD_CATEGORY_FORM_NAME } from '../consts';
 
 const AddCategoryForm = () => {
+	const rightSectionButtons = [
+		<Button
+			label="!!добавить"
+			type="primary"
+		/>,
+		<Button
+			label="!!добавить"
+			type="secondary"
+			transparent
+		/>
+	];
+
 	return (
 		<Form
 			name={ADD_CATEGORY_FORM_NAME}
 		>
-			<FormLayout title="!!! Добавить категорию">
-				<FormLayoutItem>
-					<Field
-						component={Textbox}
-						name="age"
-					/>
-				</FormLayoutItem>
-				<FormLayoutItem>
-					<Field
-						component={Dropdown}
-						name="dropdown"
-					/>
-				</FormLayoutItem>
+			<FormLayout>
+				<FormLayoutHeader title="!!! Добавить категорию" />
+				<FormLayoutItemGroup>
+					<FormLayoutItem>
+						<Field
+							component={Textbox}
+							name="age"
+						/>
+					</FormLayoutItem>
+					<FormLayoutItem>
+						<Field
+							component={Dropdown}
+							name="dropdown"
+						/>
+					</FormLayoutItem>
+				</FormLayoutItemGroup>
+				<FormLayoutFooter>
+					<ButtonGroup rightButtons={rightSectionButtons} />
+				</FormLayoutFooter>
 			</FormLayout>
 		</Form>
 	);
