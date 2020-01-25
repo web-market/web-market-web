2fb3a8dc6ddb8ae2cc543f8fd6af50b0dc647d38
kostrebadv <kostrebadd@gmail.com>
2020-01-25 13:01:13 +0200

    Categories. Actions.

--- client/src/components/Admin/Categories/store/CategoriesContext.js
+++ client/src/components/Admin/Categories/store/CategoriesContext.js
@@ -1,7 +1,6 @@
 import React, { createContext, useReducer } from 'react';
 
 import { reducer, initialState } from './reducer';
-import { SET_CATEGORIES, SET_PENDING } from './consts';
 import moduleActions from './actions';
 
 export const CategoriesContext = createContext(initialState);
@@ -9,31 +8,11 @@ export const CategoriesContext = createContext(initialState);
 export const CategoriesContextProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
 
-	const setCategories = (categories) => {
-		dispatch({
-			type: SET_CATEGORIES,
-			categories
-		});
-	};
-
-	const setPending = (pending) => {
-		dispatch({
-			type: SET_PENDING,
-			pending
-		});
-	};
-
-	const reducerActions = {
-		setPending,
-		setCategories
-	};
-
 	return (
 		<CategoriesContext.Provider
 			value={{
 				...state,
-				...reducerActions,
-				...moduleActions(reducerActions),
+				...moduleActions(dispatch),
 				dispatch,
 			}}
 		>
--- client/src/components/Admin/Categories/store/actions.js
+++ client/src/components/Admin/Categories/store/actions.js
@@ -1,6 +1,21 @@
 import { addCategory, getAllCategories } from '../api';
+import { SET_CATEGORIES, SET_PENDING } from './consts';
 
-export default ({ setCategories, setPending }) => {
+export default (dispatch) => {
+	const setCategories = (categories) => {
+		dispatch({
+			type: SET_CATEGORIES,
+			categories
+		});
+	};
+
+	const setPending = (pending) => {
+		dispatch({
+			type: SET_PENDING,
+			pending
+		});
+	};
+
 	return {
 		getCategoriesList: () => {
 			setPending(true);
